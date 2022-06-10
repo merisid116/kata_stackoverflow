a2bc10f4a9f824198589e824ca5297cd90f5014a
Vadim Shkabaro <spasovecvadim@gmail.com>
2022-06-10 10:42:40 +0000

    Resolve "Реализовать методы в классах ReadWriteServiceImpl и ReadOnlyServiceImpl"

--- src/main/java/com/javamentor/qa/platform/service/impl/model/ReadOnlyServiceImpl.java
+++ src/main/java/com/javamentor/qa/platform/service/impl/model/ReadOnlyServiceImpl.java
@@ -1,6 +1,48 @@
 package com.javamentor.qa.platform.service.impl.model;
 
-public abstract class ReadOnlyServiceImpl<E, K> {
+import com.javamentor.qa.platform.dao.abstracts.model.ReadOnlyDao;
+import com.javamentor.qa.platform.service.abstracts.model.ReadOnlyService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
 
+@Service
+@Transactional
+public abstract class ReadOnlyServiceImpl<E, K> implements ReadOnlyService<E, K> {
+
+    private final ReadOnlyDao readOnlyDao;
+
+    @Autowired
+    public ReadOnlyServiceImpl(ReadOnlyDao readOnlyDao) {
+        this.readOnlyDao = readOnlyDao;
+    }
+
+    @Override
+    public List<E> getAll() {
+        return readOnlyDao.getAll();
+    }
+
+    @Override
+    public boolean existsById(K id) {
+        return readOnlyDao.existsById(id);
+    }
+
+    @Override
+    public Optional<E> getById(K id) {
+        return readOnlyDao.getById(id);
+    }
+
+    @Override
+    public List<E> getAllByIds(Iterable<K> ids) {
+        return readOnlyDao.getAllByIds(ids);
+    }
+
+    @Override
+    public boolean existsByAllIds(Collection<K> ids) {
+        return readOnlyDao.existsByAllIds(ids);
+    }
 }
